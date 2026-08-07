17a24b37ad0c2c50fb3b9e36c50ec90ff4aba1c5
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2026-08-07 16:09:12 -0700

    expanded test script

--- tests/sim.py
+++ tests/sim.py
@@ -1,14 +1,18 @@
 import subprocess
 
+ENVIRONMENTS = ["square"]
+NUMBER_OF_ROBOTS = [2, 4]
 NUMBER_OF_TESTS = 2
 
-for i in range(NUMBER_OF_TESTS):
-    p = subprocess.Popen(
-        ["../build/ExplorationBot", "square", "2", "test"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        text=True,
-        bufsize=1,
-    )
+for env in ENVIRONMENTS:
+    for bots in NUMBER_OF_ROBOTS:
+        for i in range(NUMBER_OF_TESTS):
+            p = subprocess.Popen(
+                ["../build/ExplorationBot", env, str(bots), "test"],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                text=True,
+                bufsize=1,
+            )
 
-    p.wait()
+            p.wait()
