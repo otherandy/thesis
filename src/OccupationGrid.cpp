5f9548840e96a67625e78a3d75de8725645ce9d7
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2026-09-01 13:59:56 -0700

    fixed dead frontier cell check

--- src/OccupationGrid.cpp
+++ src/OccupationGrid.cpp
@@ -190,18 +190,7 @@ void OccupationGrid::remove_dead_frontier_cells() {
         continue;
       }
 
-      if (c->frontier_id != std::nullopt) {
-        continue;
-      }
-
-      int frontier_neighbors = 0;
-      for (auto n : c->get_neighbors(&grid)) {
-        if (n->state == CellState::Frontier) {
-          frontier_neighbors++;
-        }
-      }
-
-      if (frontier_neighbors <= 1) {
+      if (!has_neighbor_state(Index2D{y, x}, CellState::Unknown)) {
         c->state = CellState::Free;
         frontier_cell_count--;
       }
@@ -213,7 +202,7 @@ void OccupationGrid::compute_frontier_regions(DynamicScheduler *sched) {
   std::vector<std::shared_ptr<FrontierRegion>> regions;
   std::unordered_set<Cell *> global_visited;
 
-  // remove_dead_frontier_cells();
+  remove_dead_frontier_cells();
 
   auto is_closed = [&](std::vector<Cell *> region, Index2D min,
                        Index2D max) -> bool {
